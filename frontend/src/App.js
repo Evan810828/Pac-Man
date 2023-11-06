3312877d4862c6afcd16d6ceb2cb72dc977b2adc
Evan810828 <yw185@rice.edu>
2023-11-05 20:11:35 -0600

    init

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Pacman from 'react-pacman';
+
+function App() {
+  return (
+    <div className="App">
+      <Pacman />
+    </div>
+  );
+}
+
+export default App;
